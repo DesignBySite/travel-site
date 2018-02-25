eb248764d22971845dfe2b70e4358b06ed9f0198
Kevin Nielsen <knielsen0506@gmail.com>
2018-02-25 15:38:32 -0600

    added postcss-nested and import to require and pipe

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp         = require('gulp'),
     postcss      = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars      = require('postcss-simple-vars'),
-    nested       = require('postcss-nested');
+    nested       = require('postcss-nested'),
+    cssImport    = require('postcss-import');
 
 
 gulp.task('default', function(){
@@ -11,12 +12,12 @@ gulp.task('default', function(){
 });
 
 gulp.task('html', function(){
-  console.log("Blahhhhhh Gulppity");
+  console.log("Started HTML");
 });
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
